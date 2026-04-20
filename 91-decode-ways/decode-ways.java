0aea4c4829c912d83fcd1364aaeae8db6ea524f2
Alok-Raghuvanshi <alokraghuvanshi837@gmail.com>
2026-04-20 13:47:42 +0530

    Time: 1 ms (84.78%) | Memory: 43.1 MB (67.81%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numDecodings(String s) {
+      int n=s.length();
+      int dp[]=new int[n+1];
+      dp[0]=1;
+      dp[1]=(s.charAt(0)=='0')?0:1;
+
+      for(int i=2;i<=n;i++)
+      {
+        int onedigit=s.charAt(i-1)-'0';
+        int twodigit=(s.charAt(i-2)-'0')*10+onedigit;
+         
+         if(onedigit>=1)
+         dp[i]=dp[i]+dp[i-1];
+
+         if(twodigit>=10&&twodigit<=26)
+         dp[i]=dp[i]+dp[i-2];
+      }
+      return dp[n];
+    }
+}
