ae1535e4f29ee8683f976799bc48af3a3e0b798b
Alok Raghuvanshi <alokraghuvanshi837@gmail.com>
2026-07-10 00:05:47 +0530

    Time: 7 ms (12.72%) | Memory: 45.9 MB (53.46%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=x+"";
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+        while(i<=j)
+        {
+         if(s.charAt(i)!=s.charAt(j))
+         return false; 
+         i++;
+         j--;  
+        }
+        return true;
+    }
+}
